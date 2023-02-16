c822a095017a17d3551d0917d8adf5940fdeeb7c
mrludovicc <nadal46@hotmail.com>
2023-02-15 19:18:33 -0500

    cleaning the code with eslint and fixing it

--- fetcher.js
+++ fetcher.js
@@ -13,37 +13,37 @@ request(args[0], (error, response, body) => {
     return false;
   }
   if (error) {
-    console.log(error)
-    process.exit()
+    console.log(error);
+    process.exit();
   }
   if (!response) {
-    console.log('No response')
-
+    console.log('No response');
+    process.exit();
   }
   const content = body;
 
   if (fs.existsSync(args[1])) {
     rl.question("file exists: would you like to overwrite? ", (answer) => {
       if (answer.toLowerCase() !== 'y') {
-        rl.close()
-        process.exit()
+        rl.close();
+        process.exit();
       } else {
-        writeFile(args[1], content)
+        writeFile(args[1], content);
       }
-    })
+    });
   } else {
-    writeFile(args[1], content)
+    writeFile(args[1], content);
   }
-})
+});
 const writeFile = (local, content) => {
   fs.writeFile(local, content, err => {
     if (err) {
       console.error(err);
-      process.exit()
+      process.exit();
     }
-    const stats = fs.statSync(local)
+    const stats = fs.statSync(local);
     const size = stats.size;
-    console.log(`Downloaded and saved ${size} bytes to ${local}`)
-    process.exit()
+    console.log(`Downloaded and saved ${size} bytes to ${local}`);
+    process.exit();
   });
-}
+};
